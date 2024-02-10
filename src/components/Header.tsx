b29f958fb5f61a63219c94afda82da9a4fe9ad6f
Lynn <lynnkinsella2000@gmail.com>
2024-02-10 18:25:47 -0500

    preloading svg logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
-// import silkendev from "../assets/silkendev.svg"
-import ReactComponent from "../assets/silkendev.svg";
+import silkendev from "../assets/silkendev.svg"
 
 export function Header() {
     return (
@@ -14,8 +13,7 @@ function LogoBadge({ }) {
     return (
         <Link to="/"
             className="py-4 px-5 cursor-pointer border border-black rounded-2xl h-[100%] bg-gradient-to-t to-white from-yellow-100">
-            {/* <img className="h-[100%]" src={silkendev} loading="eager" alt="logo" /> */}
-            <ReactComponent></ReactComponent>
+            <img className="h-[100%]" src={silkendev} alt="logo" rel="pre" />
 
         </Link>
     )
