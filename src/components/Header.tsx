351c8525b68fdd1367845be3c3e888e073e311b4
Lynn <lynnkinsella2000@gmail.com>
2024-02-10 18:48:30 -0500

    fix public location

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import silkendev from "public/silkendev.svg"
+// import silkendev from "public/silkendev.svg"
 
 export function Header() {
     return (
@@ -13,7 +13,7 @@ function LogoBadge({ }) {
     return (
         <Link to="/"
             className="py-4 px-5 cursor-pointer border border-black rounded-2xl h-[100%] bg-gradient-to-t to-white from-yellow-100">
-            <img className="h-[100%]" src={silkendev} alt="logo" rel="pre" />
+            <img className="h-[100%]" src={"public/silkendev.svg"} alt="logo" rel="pre" />
         </Link>
     )
 }
