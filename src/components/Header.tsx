45c71377e25f679deb6dea32e7eed6969b3f7382
Lynn <lynnkinsella2000@gmail.com>
2024-02-10 18:27:21 -0500

    test no pre load, raw svg load

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ function LogoBadge({ }) {
     return (
         <Link to="/"
             className="py-4 px-5 cursor-pointer border border-black rounded-2xl h-[100%] bg-gradient-to-t to-white from-yellow-100">
-            <img className="h-[100%]" src={silkendev} alt="logo" rel="pre" />
+            <img className="h-[100%]" src={silkendev} alt="logo" />
 
         </Link>
     )
