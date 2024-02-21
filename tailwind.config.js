3cff73365e70a85d80ef23f9527dbe31b4564f94
Lynn <lynnkinsella2000@gmail.com>
2024-02-21 12:26:05 -0500

    Changed Help box heiryarchy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/pages/ui/*.tsx",
-    "./src/pages/**/**/*.tsx",
+    "./src/pages/**/ui/*.tsx",
+    "./src/pages/**/*.tsx",
     "./src/components/*.tsx",
   ],
   safelist: [
